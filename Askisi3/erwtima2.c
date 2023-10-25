d6d5c48102438eb44d81a3160f659a41a228664d
Pavlos Skourtis <72971330+ntua-el20052@users.noreply.github.com>
2023-10-25 16:25:24 +0300

    Add files via upload

--- Askisi3/erwtima2.c
+++ Askisi3/erwtima2.c
@@ -2,44 +2,70 @@
 #include "avr/io.h"
 #include "util/delay.h"
 
+void adc_init(){
+    return;
+}
 
-int main(){
-    
-    TCCR1A =(1 << WGM10) | (1 << COM1A1);
-    TCCR1B =(1 << WGM12) | (1 << CS11);
-    ADMUX |=0b01100001;
-    ADCSRA |=0b10000111;
-    unsigned char duty=128;
-    DDRC |=0b00111111; //C output
-    DDRB |=0b00111111; //B output
-    DDRD |=0b00000000; //D inpou
-    while(1){
-        
-        unsigned char inpout,temp,adcvalue;
-        inpout = PIND;
-        _delay_ms(100);
-        if(inpout == 0b11111110){if (duty <228)duty=227;} // != exw arnitiki logiki
-        if(inpout == 0b11111101){if (duty > 0)duty=1;}  // != exw arnitiki logiki
-        
-        OCR1AL=duty;
-        
-        
-        ADCSRA|=(1 << ADSC);
+int calc(){
+    ADCSRA|=(1 << ADSC);
+    unsigned char temp; 
         while(1){
             temp=ADCSRA;
             temp=temp&0b01000000;
             if(temp==0b00000000){
                 break;
             }
-        }
-        adcvalue=ADCL;
-        if(adcvalue>0b10000000){PORTC=0b00011111;}
-        else if(adcvalue<0b10000000){PORTC=0b00101111;}
-        }
+       }
+
+}
+
+
+int open(unsigned char duty){
+        OCR1AL=duty;
+        unsigned char adcvalueh,adcvaluel;
+        adcvaluel=ADCL;
+        adcvalueh=ADCH;
+       //PORTD = adcvalueh ;
+        //PORTD =  ADCH;
+        if(adcvalueh>0x00 && adcvalueh<=0x20){PORTD=0b00000001;}
+        if(adcvalueh>0x20 && adcvalueh<=0x40){PORTD=0b00000010;}
+        if(adcvalueh>0x40 && adcvalueh<=0x60){PORTD=0b00000100;}
+        if(adcvalueh>0x60 && adcvalueh<=0x80){PORTD=0b00001000;}
+        if(adcvalueh<0x80 && adcvalueh<=0xA0){PORTD=0b00010000;}
+        if(adcvalueh>0xA0 && adcvalueh<=0xC0){PORTD=0b00100000;}
+        if(adcvalueh>0XC0 && adcvalueh<=0xE0){PORTD=0b01000000;}
+        if(adcvalueh>0xE0 && adcvalueh<=0x100){PORTD=0b10000000;}
         
-        
+
+        return 2;
+
+}
+
+
+int main(){
     
+    TCCR1A =(1 << WGM10) | (1 << COM1A1);
+    TCCR1B =(1 << WGM12) | (1 << CS11);
+    ADMUX =0b01100001;
+    ADCSRA =0b10000111;
+    unsigned char duty=134;
+    DDRB |=0b00000000; //C output
+    DDRD |=0b11111111; //B output
+    //DDRD |=0b11111111; //D inpou
+    while(1){
+        unsigned char inpout;
+       
+        
+        
+        inpout = PINB;
+        _delay_ms(100);
+        if(inpout==0b10011110){if(duty<255){duty+=20;}}// != exw arnitiki logiki
+       
+        else if(inpout==0b10011101){if(duty>13){duty-=20;} } // != exw arnitiki logiki
+        calc();
+        open(duty);
     }
+}
 
 
 
