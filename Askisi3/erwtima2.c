ce03fd131f3c24cc83471a5488a1059fef4c2a99
Pavlos Skourtis <72971330+ntua-el20052@users.noreply.github.com>
2023-10-25 11:31:13 +0300

    Add files via upload

--- Askisi3/erwtima2.c
+++ Askisi3/erwtima2.c
@@ -7,20 +7,39 @@ int main(){
     
     TCCR1A =(1 << WGM10) | (1 << COM1A1);
     TCCR1B =(1 << WGM12) | (1 << CS11);
+    ADMUX |=0b01100001;
+    ADCSRA |=0b10000111;
     unsigned char duty=128;
     DDRC |=0b00111111; //C output
     DDRB |=0b00111111; //B output
-    DDRD |=0b00000000; //D inpout
+    DDRD |=0b00000000; //D inpou
     while(1){
-        unsigned char inpout;
+        
+        unsigned char inpout,temp,adcvalue;
         inpout = PIND;
-        if(inpout == 0b11111101){if (duty <228)duty+=20;} // != exw arnitiki logiki
-        if(inpout == 0b11111011){if (duty > 28)duty-=20;}  // != exw arnitiki logiki
-        PINC = PINB;
+        _delay_ms(100);
+        if(inpout == 0b11111110){if (duty <228)duty=227;} // != exw arnitiki logiki
+        if(inpout == 0b11111101){if (duty > 0)duty=1;}  // != exw arnitiki logiki
+        
         OCR1AL=duty;
-    
+        
+        
+        ADCSRA|=(1 << ADSC);
+        while(1){
+            temp=ADCSRA;
+            temp=temp&0b01000000;
+            if(temp==0b00000000){
+                break;
+            }
+        }
+        adcvalue=ADCL;
+        if(adcvalue>0b10000000){PORTC=0b00011111;}
+        else if(adcvalue<0b10000000){PORTC=0b00101111;}
+        }
+        
+        
     
     }
 
 
-}
+
