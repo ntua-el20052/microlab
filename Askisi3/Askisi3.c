7b8353cfc7236c4ccdf75be18530456fd2b031f0
ConstantinePapadopoulos <147601871+ConstantinePapadopoulos@users.noreply.github.com>
2023-10-26 00:51:03 +0300

    Update Askisi3.c

--- Askisi3/Askisi3.c
+++ Askisi3/Askisi3.c
@@ -2,6 +2,10 @@
 #include "avr/io.h"
 #include "util/delay.h"
 
+uint8_t duty=134;
+//GLOBAL
+
+
 void adc_init(){
    
     return;
@@ -12,41 +16,67 @@ uint8_t table [9]={0, 32, 64, 96. 128, 160, 192, 224,255};
 // i idea einai sto if na valoume ena switch me to table alla variemai twra, isws to valw chat gpt na to vgalei
 
 void open(){
-        ADCSRA |=(1 << ADSC);
-        uint8_t temp; 
-        while(1){
-            temp=ADCSRA;
-            temp=temp & 64; //0b01000000; apomonosi ADSC
-            if(temp==0)break;
-       }
-        uint8_t adcvalueh,adcvaluel;
-        adcvaluel=ADCL;
-        adcvalueh=ADCH;
-        //ORTD=adcvalueh;
-       //PORTD = adcvalueh ;
-        //PORTD =  ADCH;
-        if(adcvalueh>0 && adcvalueh<=32){PORTD=0b00000001;}
-        if(adcvalueh>32 && adcvalueh<=64){PORTD=0b00000010;}
-        if(adcvalueh>64 && adcvalueh<=96){PORTD=0b00000100;}
-        if(adcvalueh>96 && adcvalueh<=128){PORTD=0b00001000;}
-        if(adcvalueh>128 && adcvalueh<=160){PORTD=0b00010000;}
-        if(adcvalueh>160 && adcvalueh<=192){PORTD=0b00100000;}
-        if(adcvalueh>192 && adcvalueh<=224){PORTD=0b01000000;}
-        if(adcvalueh>224 && adcvalueh<=255){PORTD=0b10000000;}
         
 
-        return;
+}
+
+void mode1(){
+   uint8_t inpout;
+   input = PINB;
+   _delay_ms(100);            //delay kuriws gia debouncing
+   input = input & 6;    //apomonosi button p1-p2 as 0b00110000 = 48
+   if(input==2)        {if(duty<245){duty+=20;OCR1AL=duty;} }// != exw arnitiki logiki 
+   else if(input==4)   {if(duty>25 ){duty-=20;OCR1AL=duty;} } // != exw arnitiki logiki
 
 }
+
+void mode2(){
+//while(1){                                           //auto to while me to apokatw sxolia- isws xreiastei, trexei mexri na pati8ei allagi mode wste na glitwsw calls sth sinartisi mode2 
+   ADCSRA |=(1 << ADSC);
+   uint8_t temp; 
+   while(1){
+      temp=ADCSRA;
+      temp=temp & 64; //0b01000000; apomonosi ADSC
+      if(temp==0)break;
+   }
+   uint8_t adcvalueh,adcvaluel, duty2;
+   adcvaluel=ADCL;
+   adcvalueh=ADCH;
+   if(adcvalueh>0 && adcvalueh<=32){duty=20;}
+   if(adcvalueh>32 && adcvalueh<=64){duty=50;}
+   if(adcvalueh>64 && adcvalueh<=96){duty=80;}
+   if(adcvalueh>96 && adcvalueh<=128){duty=110;}
+   if(adcvalueh>128 && adcvalueh<=160){duty=140;;}
+   if(adcvalueh>160 && adcvalueh<=192){duty=170;}
+   if(adcvalueh>192 && adcvalueh<=224){duty=200;}
+   if(adcvalueh>224 && adcvalueh<=255){duty=250;}
+   OCR1AL=duty;     
+
+   return;
+
+//uint8_t inpout;
+//input = PIND;
+//_delay_ms(100);            //delay kuriws gia debouncing
+//input = input & 192;    //apomonosi button p6-p7 as 0b11000000 = 192
+//if(input==64||input ==128)        {break;}//  PRESSED P1 or p2
+//}   
+}
+
+
+
+
+
+
+
 
 
 int main(){
     
     TCCR1A =(1 << WGM10) | (1 << COM1A1);
     TCCR1B =(1 << WGM12) | (1 << CS11);
-    ADMUX =0b01100001;
+    ADMUX =0b01100000; // upo8etw mas endiaferei o adc0 pou einai to pontesiometro
     ADCSRA =0b10000111;
-    uint8_t duty=134;
+    
     //OCR1AL=duty;
     DDRD |=0b00000000; //D input
     DDRB |=0b11111111; //B output
@@ -57,16 +87,16 @@ int main(){
         input = PIND;
         _delay_ms(100);            //delay kuriws gia debouncing
         input = input & 192;    //apomonosi button p6-p7 as 0b11000000 = 192
-        if(input==64)        {mode=1;}// != exw arnitiki logiki  PRESSED P1
-        else if(input==128)   {mode=2;} // != exw arnitiki logiki PRESSED P2
+        if(input==64)        {mode=1;}//  PRESSED P1
+        else if(input==128)   {mode=2;} //  PRESSED P2
 
        if (mode==1){
-          
+          mode1();
 
          
          }
      else {
-
+            mode2();
 
        
          }
